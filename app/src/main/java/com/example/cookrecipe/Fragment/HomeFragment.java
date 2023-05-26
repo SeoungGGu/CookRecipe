159c07eaff608e8e1f450f5f88a946efba20578a
SeoungGGu <b0321ok@gmail.com>
2023-05-26 10:07:11 +0900

    homedatabase success

--- app/src/main/java/com/example/cookrecipe/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/cookrecipe/Fragment/HomeFragment.java
@@ -1,19 +1,39 @@
 package com.example.cookrecipe.Fragment;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.example.cookrecipe.R;
+import com.example.cookrecipe.code.Notice;
+import com.example.cookrecipe.code.NoticeListAdapter;
 import com.example.cookrecipe.code.SessionManager;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+
+    private ListView noticeListView;
+    private NoticeListAdapter adapter;
+    private List<Notice> noticeList;
 
     private ImageButton mBtn1, mBtn2, mBtn3, mBtn4, mBtn5, mBtn6 ;
     private FragmentManager mFragmentManager;
@@ -21,18 +41,23 @@ public class HomeFragment extends Fragment {
     private String loggedInUsername;
     private TextView dbtest;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        noticeListView = (ListView) view.findViewById(R.id.noticeListView);
+        noticeList = new ArrayList<Notice>();
+        adapter = new NoticeListAdapter(getActivity(), noticeList);
+        noticeListView.setAdapter(adapter);
         return view;
+
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        dbtest = view.findViewById(R.id.dbtest);
         mBtn1 = view.findViewById(R.id.button1);
         mBtn2 = view.findViewById(R.id.button2);
         mBtn3 = view.findViewById(R.id.button3);
@@ -45,7 +70,6 @@ public class HomeFragment extends Fragment {
 
         if (sessionManager.isLoggedIn()) {
             loggedInUsername = sessionManager.getUsername();
-            dbtest.setText(loggedInUsername);
         } else {
             // Handle the case when the user is not logged in
             // Redirect to the login activity or show an error message
@@ -106,7 +130,67 @@ public class HomeFragment extends Fragment {
                         .commit();
             }
         });
+
+        new BackgroundTask().execute();
     }
 
+    class BackgroundTask extends AsyncTask<Void, Void, String> {
+
+        String target;
+
+        @Override
+        protected void onPreExecute() {
+            target = "http://192.168.219.103/AndroidDB/NoticeList.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try{
+                URL url = new URL (target);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while((temp = bufferedReader.readLine()) != null){
+                    stringBuilder.append(temp + "\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onProgressUpdate(Void... values) {
+            super.onProgressUpdate();
+        }
+
+        @Override
+        public void onPostExecute(String result){
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+                int count = 0;
+                String noticeContent, noticeName, noticeDate;
+                while(count < jsonArray.length()){
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    noticeContent = object.getString("noticeContent");
+                    noticeName = object.getString("noticeName");
+                    noticeDate = object.getString("noticeDate");
+                    Notice notice = new Notice(noticeContent, noticeName, noticeDate);
+                    noticeList.add(notice);
+                    adapter.notifyDataSetChanged();
+                    count++;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
 
